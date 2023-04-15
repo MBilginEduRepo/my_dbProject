83b6537af0fe0064d709131b5b58ff792d9ff900
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-04-15 18:14:41 +0300

    day2.sql is updated and day3.sql and day4.sql are created

--- Queries/day2.sql
+++ Queries/day2.sql
@@ -1,1 +1,25 @@
---get me average salary for IY_PROG
+--get me unique JOB_IDS
+select distinct JOB_ID from EMPLOYEES;
+
+--get me average salary for IT_PROG
+select avg(SALARY) from EMPLOYEES
+where JOB_ID = 'IT_PROG';
+
+select JOB_ID,avg(SALARY),count(*),sum(SALARY) from EMPLOYEES
+group by JOB_ID;
+
+--get me JOB_IDS where their avg salary is more than 5k
+select JOB_ID,avg(SALARY),count(*),sum(SALARY) from EMPLOYEES
+where SALARY >5000
+group by JOB_ID;
+
+--get me JOB_IDS where their avg salary is more than 5k
+select JOB_ID,avg(SALARY),count(*),sum(SALARY) from EMPLOYEES
+group by JOB_ID
+having avg(SALARY) > 5000;
+
+
+
+
+
+
---
+++ Queries/day3.sql
@@ -0,0 +1,42 @@
+--how to find employees information of who is making highest salary in the company
+
+--get me the highest salary
+select max(SALARY) from EMPLOYEES;
+
+--highest salary employee information
+select * from EMPLOYEES
+where SALARY = 24000;
+
+--subquery
+select * from EMPLOYEES
+where SALARY = (select max(SALARY) from EMPLOYEES);
+
+--finding second highest salary
+
+--get highest salary first
+select max(SALARY) from EMPLOYEES;
+
+--highest after 24k
+select max(SALARY) from EMPLOYEES
+where SALARY < 24000;
+
+--one shot combining two queries
+select max(SALARY) from EMPLOYEES
+where SALARY < (select max(SALARY) from EMPLOYEES);
+
+--employee information of who is making second highest salary ?
+select * from EMPLOYEES
+where SALARY=(select max(SALARY) from EMPLOYEES
+       where SALARY < (select max(SALARY) from EMPLOYEES));
+
+select * from EMPLOYEES
+where rownum < 11;
+
+--list the employees who is making top 10 salary
+select * from EMPLOYEES
+where ROWNUM < 11
+order by SALARY desc;
+
+--order all employees based on salary high to low then display only first 10 result
+select * from (select * from EMPLOYEES order by SALARY desc)
+where rownum < 11;
