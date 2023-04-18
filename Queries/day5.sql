06c62ac437e47581e9e77ec414fb200b427aab25
yavuz1990 <115427573+yavuz1990@users.noreply.github.com>
2023-04-18 16:57:24 +0300

    day5.sql is completed

--- Queries/day5.sql
+++ Queries/day5.sql
@@ -1,3 +1,17 @@
 --find the highest 14th salary
-select min(SALARY) from(select * from EMPLOYEES order by SALARY desc )
+--list salary higgh to low without duplicate values
+select distinct SALARY
+from EMPLOYEES
+order by SALARY desc;
+
+--find the highest 14th salary(removing duplicate values)
+select min(SALARY)
+from (select distinct SALARY from EMPLOYEES order by SALARY desc)
 where ROWNUM < 15;
+
+--find employee info who is making 14th highest salary
+select *
+from EMPLOYEES
+where SALARY = (select min(SALARY)
+                from (select distinct SALARY from EMPLOYEES order by SALARY desc)
+                where ROWNUM < 15);
